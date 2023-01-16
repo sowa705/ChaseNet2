1c46dc009c5f3f8819abb6ab77f240ce8931612e
Łukasz Sowa <sowa705@gmail.com>
2023-01-16 01:23:38 +0100

    Demo file transfer app and a simple dumb tracker demo project

---
+++ ChaseNet2.FileTransfer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using System.Net;
+using ChaseNet2.FileTransfer;
+using ChaseNet2.Session;
+using ChaseNet2.Transport;
+using Mono.Nat;
+using Serilog;
+using Serilog.Core;
+
+public class Program
+{
+    static bool Ready = false;
+    static ConnectionManager Manager;
+    public static async Task Main(string[] args)
+    {
+        Logger logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        Log.Logger = logger;
+        
+        Manager = new ConnectionManager();
+        await Manager.Update();
+
+        // hehe
+        var trackerConnection = Manager.CreateConnection(IPEndPoint.Parse("13.80.242.30:2137"));
+
+        SessionClient client = new SessionClient("TrackerSession",Manager,trackerConnection);
+        
+        client.Connect();
+        
+        int counter=0;
+        while (true)
+        {
+            if (counter%30==0)
+            {
+                Console.WriteLine($"Host statistics: {Manager.Statistics}");
+            }
+            
+            await Task.Delay(100);
+            await Manager.Update();
+            counter++;
+        }
+    }
+}
---
+++ ChaseNet2.SimpleTracker/Program.cs
@@ -0,0 +1,31 @@
+﻿using ChaseNet2.Session;
+using ChaseNet2.Transport;
+using Serilog;
+using Serilog.Core;
+
+Logger logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .CreateLogger();
+
+Log.Logger = logger;
+
+ConnectionManager cm = new ConnectionManager(2137);
+cm.AcceptNewConnections = true;
+
+SessionTracker st = new SessionTracker(cm);
+
+st.SessionName = "TrackerSession";
+
+int counter=0;
+while (true)
+{
+    if (counter%30==0)
+    {
+        Console.WriteLine($"Host statistics: {cm.Statistics}");
+    }
+            
+    await Task.Delay(100);
+    await cm.Update();
+    counter++;
+}
